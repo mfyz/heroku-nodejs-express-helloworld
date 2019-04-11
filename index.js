6931773d3dcb1429cc08b4a8e462f8703b39118e
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-04-11 12:33:51 -0700

    express fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
